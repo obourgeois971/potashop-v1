609f27dec8a5b9b43bdc25df52185cb45f1748fb
Olivier <bourgeoisolivier@hotmail.fr>
2023-11-16 16:41:12 +0100

    Ajout de slugify

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,6 +1,20 @@
+import Category from "../models/category.js";
+import slugify from "slugify";
+
 export const create = async (req, res) => {
   try {
-    console.log(req.body);
+    const { name } = req.body;
+    if (!name.trim()) {
+      return res.json({ error: "Name is required" });
+    }
+    const existingCategory = await Category.findOne({ name });
+    if (existingCategory) {
+      return res.json({ error: "Already exists" });
+    }
+
+    const category = await new Category({ name, slug: slugify(name) }).save();
+
+    res.json(category);
   } catch (err) {
     console.log(err);
     return res.status(400).json(err);
