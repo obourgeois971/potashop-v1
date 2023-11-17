2be8c8f9fb2537630ff72c5e9b2481a047c489e7
Olivier <bourgeoisolivier@hotmail.fr>
2023-11-17 08:42:25 +0100

    ajout model products

---
+++ server/models/product.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      require: true,
+      maxlength: 160,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      require: true,
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      require: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      require: true,
+    },
+    quantity: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      date: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      require: false,
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
