1e4264fbfd213d242bf2e364141206d2d592f6f2
Olivier <bourgeoisolivier@hotmail.fr>
2023-11-18 09:38:58 +0100

    Init React, routes pages and bootstrap

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,9 @@
+import Jumbotron from "../components/cards/Jumbotron";
+
 export default function Home() {
   return (
     <div>
+      <Jumbotron />
       <h1>This is Home component</h1>
     </div>
   );
