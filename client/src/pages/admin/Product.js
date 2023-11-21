b56fb4cd7191dbe1a94b5e7820277bbaa1f2a123
Olivier <bourgeoisolivier@hotmail.fr>
2023-11-21 19:16:20 +0100

    add categori, product form

--- client/src/pages/admin/Product.js
+++ client/src/pages/admin/Product.js
@@ -1,10 +1,36 @@
+import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import Jumbotron from "../../components/cards/Jumbotron";
 import AdminMenu from "../../components/nav/AdminMenu";
+import axios from "axios";
+import { Select } from "antd";
+const { Option } = Select;
 
 export default function AdminProduct() {
   // context
   const [auth, setAuth] = useAuth();
+  //state
+  const [categories, setCategories] = useState([]);
+  const [photo, setPhoto] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get("/categories");
+      setCategories(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -20,9 +46,46 @@ export default function AdminProduct() {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <div className="p-3 mt-2 mb-2 h4 bg-light">Manage Products</div>
+            <div className="p-3 mt-2 mb-2 h4 bg-light">Create Products</div>
 
-            <p>Create products from...</p>
+            {photo && (
+              <div className="text-center">
+                <img
+                  src={URL.createObjectURL(photo)}
+                  alt="product photo"
+                  className="img img-responsive"
+                  height="200px"
+                />
+              </div>
+            )}
+
+            <div className="pt-2">
+              <label className="btn btn-outline-secondary col-12 mb-3">
+                {photo ? photo.name : "Upload photo"}
+                <input
+                  type="file"
+                  name="photo"
+                  accept="image/*"
+                  onChange={(e) => setPhoto(e.target.files[0])}
+                  hidden
+                />
+              </label>
+            </div>
+
+            <Select
+              showSearch
+              bordered={false}
+              size="large"
+              className="form-select mb-3"
+              placeholder="Choose category"
+              onChange={(value) => setCategories(value)}
+            >
+              {categories?.map((c) => (
+                <Option key={c._id} value={c.name}>
+                  {c.name}
+                </Option>
+              ))}
+            </Select>
           </div>
         </div>
       </div>
